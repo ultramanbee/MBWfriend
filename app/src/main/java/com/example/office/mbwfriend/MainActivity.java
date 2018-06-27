282c9a659ffe29ac9199ddc8572b3e10b8313072
patipat <patipat@mbw.ac.th>
2018-06-27 16:23:30 +0700

    Finish 27June

--- app/src/main/java/com/example/office/mbwfriend/MainActivity.java
+++ app/src/main/java/com/example/office/mbwfriend/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.office.mbwfriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.office.mbwfriend.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+
+        }
+    }  //Main Method
+
+
+}  //Main Class
