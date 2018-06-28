b5a40c7d8ba77d74b55a60c9ca0b1f180f5bcaf5
patipat <patipat@mbw.ac.th>
2018-06-28 16:35:07 +0700

    Finish 28June Complet

--- app/src/main/java/com/example/office/mbwfriend/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/office/mbwfriend/fragment/RegisterFragment.java
@@ -1,5 +1,9 @@
 package com.example.office.mbwfriend.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,17 +12,61 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.office.mbwfriend.MainActivity;
 import com.example.office.mbwfriend.R;
 
 public class RegisterFragment extends Fragment{
 
+    private ImageView imageView;
+    private Uri uri;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
+        createToolbar();
+
+//        Avatar Controller
+        imageView = getView().findViewById(R.id.imvavatar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Choose App"), 1);
+            }
+        });
+
+
+    } //Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == getActivity().RESULT_OK){
+
+            uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }  //if
+
+    } //Result
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity) getActivity()).setSupportActionBar(toolbar);
 
@@ -31,9 +79,7 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-
-    } //Main Method
+    }
 
     @Nullable
     @Override
